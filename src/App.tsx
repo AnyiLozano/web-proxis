dde1ac152c5fdffd918d4621742d3b7911215596
AnyiLozano <anyi.lozano2119@gmail.com>
2021-11-29 19:18:33 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {FC} from "react";
+import {PersistGate} from "redux-persist/integration/react";
+import useStore from "config/redux";
+import {Provider} from "react-redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App: FC = (): JSX.Element => {
+    const {store, persistor} = useStore();
+
+    return (
+        <React.Fragment>
+            <React.Suspense fallback={<p>...Cargando</p>}>
+                <Provider store={store}>
+                    <PersistGate persistor={persistor} loading={false}>
+                        <span>Ba ?</span>
+                    </PersistGate>
+                </Provider>
+            </React.Suspense>
+        </React.Fragment>
+    );
 }
 
 export default App;
---
+++ src/models/reducers/index.ts
@@ -0,0 +1,9 @@
+import {combineReducers} from "@reduxjs/toolkit";
+
+const useReducers = () => {
+    return combineReducers(
+        Object.assign({})
+    );
+}
+
+export default useReducers;
