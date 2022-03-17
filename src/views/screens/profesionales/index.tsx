75cfa9944320be3c24ef28d72947806a04a45e3b
Anyi Lozano <anyi.lozano2119@gmail.com>
2022-03-17 08:13:36 -0500

    cambio reset

--- src/views/screens/profesionales/index.tsx
+++ src/views/screens/profesionales/index.tsx
@@ -1,48 +1,85 @@
 import { Grid } from "@mui/material";
-import React, { FC } from "react";
+import useControllers from "controllers";
+import useModels from "models";
+import React, { FC, useState } from "react";
+import { useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
+import Login from "../Login";
+import useProductsStyles from "../products/products.style";
 import Cientifica from "./cientifica";
 import Informacion from "./informacion";
 import useProfesionalesStyles from "./profesionales.style";
 import Salud from "./salud";
 
 const Profesionales: FC = (): JSX.Element => {
-    // Products
-    const {
-        StyledAppBarPage,
-        StyledGrid,
-        StyledGrid2,
-        StyledGrid3
-    } = useProfesionalesStyles();
+  // Products
+  const { StyledAppBarPage, StyledGrid, StyledGrid2, StyledGrid3 } =
+    useProfesionalesStyles();
 
-    const { type } = useParams();
+  const { useSelectors } = useModels();
+  const { useAuthSelectors } = useSelectors();
+  const { loginSelector } = useAuthSelectors();
+  const login = useSelector(loginSelector);
+  const { StyledImage } = useProductsStyles();
 
-    return (
+  // Profesioanles
+  const { useScreenHooks } = useControllers();
+  const { useProducts } = useScreenHooks();
+  const { products } = useProducts();
+
+  const { epiprot } = products;
+
+  const obj = {
+    background:
+      "https://api-praxis.eml.com.co/wp-content/uploads/2022/03/Banner_login.png",
+    show: true,
+  };
+
+  const { type } = useParams();
+
+  const [firstImage, setFirstImage] = useState<boolean>(false);
+
+  return (
+    <React.Fragment>
+      {login.token === undefined ? (
         <React.Fragment>
-            <StyledAppBarPage position="absolute">
-                <Grid container>
-                    <StyledGrid xs={6} className="flex justify-center items-center" selected={type === "salud" && true}>
-                        <Link to="/profesionales/salud" className="text-white">
-                        Factor de Crecimiento Epidérmico
-                        </Link>
-                    </StyledGrid>
-                    <StyledGrid2 xs={6} className="flex justify-center items-center" selected={type === "cientifica" && true}>
-                        <Link to="/profesionales/cientifica" className="text-white">
-                        Evidencia científica
-                        </Link>
-                    </StyledGrid2>
-                   
-                </Grid>
-            </StyledAppBarPage>
-            {
-                type === "salud" 
-                    ? <Salud/>
-                    : type === "cientifica"
-                        ? <Cientifica/>
-                        : <Informacion/>
-            }
+          <Login {...obj} />
         </React.Fragment>
-    )
-}
+      ) : (
+        <React.Fragment>
+          <StyledAppBarPage position="absolute">
+            <Grid container>
+              <StyledGrid
+                xs={6}
+                className="flex justify-center items-center"
+                selected={type === "salud" && true}
+              >
+                <Link to="/profesionales/salud" className="text-white">
+                  Factor de Crecimiento Epidérmico
+                </Link>
+              </StyledGrid>
+              <StyledGrid2
+                xs={6}
+                className="flex justify-center items-center"
+                selected={type === "cientifica" && true}
+              >
+                <Link to="/profesionales/cientifica" className="text-white">
+                  Evidencia científica
+                </Link>
+              </StyledGrid2>
+            </Grid>
+          </StyledAppBarPage>
+          {type === "salud" ? (
+            <Salud />
+          ) : type === "cientifica" ? (
+            <Cientifica />
+          ) : (
+            <Informacion />
+          )}
+        </React.Fragment>
+      )}
+    </React.Fragment>
+  );
+};
 
 export default Profesionales;
