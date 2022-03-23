f22ec7bcaa9eb9442a57f6a0a1c06c4dd6dc388b
Anyi Lozano <anyi.lozano2119@gmail.com>
2022-03-23 01:18:33 -0500

    cambios final

--- src/views/screens/terapeutic-number/diabetes/index.tsx
+++ src/views/screens/terapeutic-number/diabetes/index.tsx
@@ -24,19 +24,37 @@ const Diabetes = () => {
     <React.Fragment>
       {woundsAssets.diabetes_banner_1 && (
         <React.Fragment>
+          {/* bannerPRINCIPAL */}
           <StyledImage
             src={woundsAssets.diabetes_banner_1.content}
             alt={woundsAssets.diabetes_banner_1.alt}
           />
+          {/* bannerPRINCIPAL */}
+
+          {/* BANNER DIABETES */}
           <StyledTerapeuticNumberSection
+            style={{ backgroundSize: "109% 85%" }}
             background={woundsAssets.diabetes_background_1.content}
           >
             <Container>
               <Grid item lg={12} className="text-center">
-                <StyledTitulo>Diabetes</StyledTitulo>
+                <StyledTitulo
+                  style={{
+                    marginTop: "0%",
+                    marginLeft: "4%",
+                    fontSize: "44px",
+                  }}
+                >
+                  Diabetes
+                </StyledTitulo>
               </Grid>
               <Grid item lg={12} className="text-center">
                 <StyledTitulo1
+                  style={{
+                    lineHeight: "1",
+                    fontSize: "18px",
+                    padding: "26px 145px",
+                  }}
                   dangerouslySetInnerHTML={{
                     __html: woundsAssets.diabetes_text_1.content,
                   }}
@@ -44,8 +62,18 @@ const Diabetes = () => {
               </Grid>
             </Container>
           </StyledTerapeuticNumberSection>
+          {/* BANNER DIABETES */}
+
+          {/* mMITOS Y VERDADES */}
           <Container maxWidth="xl">
-            <StyledSubtitle style={{ textAlign: "center" }}>
+            <StyledSubtitle
+              style={{
+                textAlign: "center",
+                marginTop: "-3%",
+                fontSize: "30px",
+                marginLeft: "7%",
+              }}
+            >
               {woundsAssets.diabetes_titulo_1 !== undefined &&
                 woundsAssets.diabetes_titulo_1.content}
             </StyledSubtitle>
@@ -59,6 +87,7 @@ const Diabetes = () => {
                           <StyledImageGeneral
                             src={item.content}
                             alt={item.alt}
+                            style={{ width: "82.8%", marginTop: "-4%" }}
                           />
                         </Grid>
                       ) : (
@@ -66,6 +95,11 @@ const Diabetes = () => {
                           <StyledImageGeneral
                             src={item.content}
                             alt={item.alt}
+                            style={{
+                              width: "82.8%",
+                              marginTop: "-4%",
+                              marginLeft: "5%",
+                            }}
                           />
                         </Grid>
                       )}
@@ -74,6 +108,9 @@ const Diabetes = () => {
                 )}
             </Grid>
           </Container>
+          {/* mMITOS Y VERDADES */}
+
+          {/* complicaciones  */}
           <StyledTerapeuticNumberSection
             style={{
               backgroundPosition: "top right",
@@ -83,18 +120,32 @@ const Diabetes = () => {
             background={woundsAssets.ulceras_background.content}
           >
             <Container>
-              <StyledSubtitle style={{ textAlign: "" }}>
+              <StyledSubtitle
+                style={{
+                  textAlign: "",
+                  marginTop: "1.5%",
+                  fontSize: "28.5px",
+                  marginLeft: "5%",
+                }}
+              >
                 {woundsAssets.diabetes_titulo_2 !== undefined &&
                   woundsAssets.diabetes_titulo_2.content}
               </StyledSubtitle>
-              <p style={{ fontSize: "20px", marginTop: "-2%" }}>
+              <p
+                style={{
+                  fontSize: "20px",
+                  marginTop: "-2.7%",
+                  lineHeight: "1",
+                  padding: "0 55px",
+                }}
+              >
                 {woundsAssets.diabetes_text_2 !== undefined &&
                   woundsAssets.diabetes_text_2.content}
               </p>
               <Grid item md={12} className="d-flex justify-center">
                 <StyledImageGeneral
                   src={woundsAssets.diabetes_imagen_1.content}
-                  style={{ width: "50%" }}
+                  style={{ width: "63.1%", marginLeft: "1.5%" }}
                   alt={woundsAssets.diabetes_imagen_1.alt}
                 />
               </Grid>
@@ -103,54 +154,100 @@ const Diabetes = () => {
                   woundsAssets.diabetes_cuadrose.map(
                     (item: ITerapeuticNumber, index: number) => (
                       <Grid item md={6} className="d-flex justify-center">
-                        <StyledImageGeneral src={item.content} alt={item.alt} />
+                        <StyledImageGeneral
+                          src={item.content}
+                          alt={item.alt}
+                          style={{
+                            width: "96%",
+                            marginTop: "2%",
+                            marginLeft: "8%",
+                          }}
+                        />
                       </Grid>
                     )
                   )}
               </Grid>
             </Container>
           </StyledTerapeuticNumberSection>
+          {/* complicaciones  */}
+
+          {/* PACIENTES DIABETES FAMILIA */}
           <Container>
             <Grid item md={12} className="d-flex justify-center">
               <StyledImageGeneral
                 src={woundsAssets.diabetes_image_2.content}
                 alt={woundsAssets.diabetes_image_2.alt}
-                style={{ width: "100%" }}
+                style={{ width: "98%", marginLeft: "6.4%", height: "463px" }}
               />
             </Grid>
-            <StyledSubtitle>
+            {/* PACIENTES DIABETES FAMILIA */}
+
+            {/* FAMILIA CUIDADOR */}
+            <StyledSubtitle
+              style={{
+                marginTop: "2.6%",
+                fontSize: "27.8px",
+                marginLeft: "5.3%",
+              }}
+            >
               {woundsAssets.diabetes_titulo_3 !== undefined &&
                 woundsAssets.diabetes_titulo_3.content}
             </StyledSubtitle>
             <Grid item md={12} className="d-flex justify-center">
               <StyledImageGeneral
+                style={{ marginTop: "-0.5%", marginLeft: "1.6%" }}
                 src={woundsAssets.diabetes_imagen_4.content}
                 alt={woundsAssets.diabetes_imagen_4.alt}
               />
             </Grid>
+            {/* FAMILIA CUIDADOR */}
           </Container>
+
+          {/* HABITOS SALUDABLES */}
           <StyledTerapeuticNumberSection
+            style={{ height: "803px" }}
             background={woundsAssets.diabetes_background_2.content}
           >
             <Container>
               <Grid item lg={12} className="">
-                <StyledTitulo>
+                <StyledTitulo
+                  style={{
+                    fontSize: "28px",
+                    marginLeft: "5.2%",
+                    marginTop: "-2.7%",
+                  }}
+                >
                   {woundsAssets.diabetes_titulo_4.content}
                 </StyledTitulo>
               </Grid>
               <Grid item lg={12} className="text-center">
-                <StyledTitulo1 style={{ marginTop: "-4%" }}>
+                <StyledTitulo1
+                  style={{
+                    lineHeight: "1",
+                    padding: "0 56px",
+                    marginTop: "0.6%",
+                  }}
+                >
                   {woundsAssets.diabetes_text_3.content}
                 </StyledTitulo1>
               </Grid>
               <Grid item md={12} className="d-flex justify-center">
                 <StyledImageGeneral
+                  style={{
+                    padding: "0 56px",
+                    marginTop: "1.6%",
+                    width: "63%",
+                    marginLeft: "6%",
+                  }}
                   src={woundsAssets.diabetes_imagen_5.content}
                   alt={woundsAssets.diabetes_imagen_5.alt}
                 />
               </Grid>
             </Container>
           </StyledTerapeuticNumberSection>
+          {/* HABITOS SALUDABLES */}
+
+          {/* ALIMENTACION ADECUADA */}
           <StyledTerapeuticNumberSection
             style={{
               backgroundPosition: "top right",
@@ -161,13 +258,17 @@ const Diabetes = () => {
           >
             <Container>
               <Grid item lg={12} className="">
-                <StyledTitulo style={{ marginTop: "5%" }}>
+                <StyledTitulo style={{ marginTop: "4%", fontSize: "28.4px" }}>
                   {woundsAssets.diabetes_titulo_5.content}
                 </StyledTitulo>
               </Grid>
               <Grid item lg={12} className="text-center">
                 <StyledTitulo1
-                  style={{ marginTop: "-4%" }}
+                  style={{
+                    marginTop: "2.4%",
+                    lineHeight: "1",
+                    padding: "0 58px",
+                  }}
                   dangerouslySetInnerHTML={{
                     __html: woundsAssets.diabetes_text_4.content,
                   }}
@@ -177,6 +278,7 @@ const Diabetes = () => {
             <Container>
               <Grid item md={12} className="d-flex justify-center">
                 <StyledImageGeneral
+                  style={{ width: "1045px", marginLeft: "3.9%" }}
                   src={woundsAssets.diabetes_imagen_6.content}
                   alt={woundsAssets.diabetes_imagen_6.alt}
                 />
@@ -185,68 +287,104 @@ const Diabetes = () => {
           </StyledTerapeuticNumberSection>
           <Container>
             <StyledTitulo1
+              style={{
+                padding: "0 58px",
+                lineHeight: "1",
+                marginTop: "39.3px",
+              }}
               dangerouslySetInnerHTML={{
                 __html: woundsAssets.diabetes_text_5.content,
               }}
             ></StyledTitulo1>
+            {/* ALIMENTACION ADECUADA */}
+
+            {/* PLATOS */}
             <Grid item md={12} className="d-flex justify-center">
               <StyledImageGeneral
+                style={{
+                  width: "798px",
+                  marginTop: "16.2px",
+                  marginLeft: "0.4%",
+                }}
                 src={woundsAssets.diabetes_imagen_7.content}
                 alt={woundsAssets.diabetes_imagen_7.alt}
               />
             </Grid>
+            {/* PLATOS */}
+
+            {/* ejercisio  */}
             <Grid container className="my-12">
               <Grid item md={6} className="d-flex justify-center">
                 <StyledImageGeneral
                   src={woundsAssets.diabetes_images_8.content}
                   alt={woundsAssets.diabetes_images_8.alt}
-                  style={{ width: "90%" }}
+                  style={{ width: "90%", marginLeft: "10%" }}
                 />
               </Grid>
               <Grid item md={6} className="d-flex justify-center">
                 <StyledImageGeneral
                   src={woundsAssets.diabetes_imagen_9.content}
-                  style={{ width: "90%" }}
+                  style={{ width: "100%", marginLeft: "9%", marginTop: "-5%" }}
                   alt={woundsAssets.diabetes_imagen_9.alt}
                 />
               </Grid>
+              {/* ejercisio  */}
             </Grid>
-            <StyledTitulo style={{ textAlign: "center" }}>
+            {/* RECOMENDACIONES */}
+            <StyledTitulo style={{ fontSize: "28.6px", marginTop: "-1%" }}>
               {woundsAssets.diabetes_titulo_6.content}
             </StyledTitulo>
+            {/* RECOMENDACIONES */}
           </Container>
+
+          {/* senor */}
           <StyledTerapeuticNumberSection
             background={woundsAssets.ulceras_background.content}
           >
             <Container maxWidth="xl">
               <Grid item md={12} className="d-flex justify-center">
                 <StyledImageGeneral
+                  style={{
+                    marginTop: "3%",
+                    width: "1008.4px",
+                    marginLeft: "42.9px",
+                  }}
                   src={woundsAssets.diabetes_imagen_10.content}
                   alt={woundsAssets.diabetes_imagen_10.alt}
                 />
               </Grid>
             </Container>
           </StyledTerapeuticNumberSection>
+          {/* senor */}
         </React.Fragment>
       )}
+
+      {/* REFERENCIAS */}
       {woundsAssets.diabetes_imagen_11 !== undefined && (
-        <div style={{ background: "#014380" }} className="py-12">
+        <div style={{ background: "#014380" }} className="py-16">
           <Container maxWidth="lg">
             <h1
               className="pb-12 text-center text-white"
-              style={{ fontSize: 30, fontWeight: "700" }}
+              style={{
+                fontSize: 30,
+                fontWeight: "700",
+                marginTop: "-3%",
+                marginLeft: "5%",
+              }}
             >
               Referencias
             </h1>
-            <p
-              dangerouslySetInnerHTML={{
-                __html: woundsAssets.diabetes_imagen_11.content,
-              }}
-              className="text-white"
-            ></p>
           </Container>
+          <p
+            dangerouslySetInnerHTML={{
+              __html: woundsAssets.diabetes_imagen_11.content,
+            }}
+            className="text-white text-xs"
+            style={{ padding: "0 160px", marginTop: "-1.2%" }}
+          ></p>
         </div>
       )}
+      {/* REFERENCIAS */}
     </React.Fragment>
   );
 };
--- src/views/screens/terapeutic-number/ulceras/index.tsx
+++ src/views/screens/terapeutic-number/ulceras/index.tsx
@@ -16,6 +16,7 @@ const Ulceras = () => {
 
   return (
     <React.Fragment>
+      {/* BANNER PRINCIPAL */}
       {woundsAssets.ulceras_Banner_1 !== undefined && (
         <StyledImage
           src={woundsAssets.ulceras_Banner_1.content}
@@ -23,24 +24,35 @@ const Ulceras = () => {
           style={{ marginBottom: "3%" }}
         />
       )}
+      {/* BANNER PRINCIPAL */}
+
+      {/* ULCERAS  */}
       {woundsAssets.ulceras_imagen_1 && (
         <React.Fragment>
           <Container maxWidth="xl">
             <Grid item lg={12} className="text-center">
-              <StyledTitulo>Úlceras</StyledTitulo>
+              <StyledTitulo
+                style={{
+                  fontSize: "45px",
+                  marginTop: "3.3rem",
+                  marginLeft: "4.4%",
+                }}
+              >
+                Úlceras
+              </StyledTitulo>
             </Grid>
             <Grid item lg={12} className="text-center">
               <img
                 src={woundsAssets.ulceras_imagen_1.content}
                 alt={woundsAssets.ulceras_imagen_1.alt}
-                style={{ width: "100%" }}
+                style={{ marginTop: "-6.7%", marginLeft: "5.3%", width: "93%" }}
               />
             </Grid>
             <Grid item lg={12} className="text-center">
               <img
                 src={woundsAssets.ulceras_imagen_2.content}
                 alt={woundsAssets.ulceras_imagen_2.alt}
-                style={{ width: "100%" }}
+                style={{ width: "100%", marginLeft: "2%", marginTop: "-0.4%" }}
               />
             </Grid>
             <StyledTerapeuticNumberSection1
@@ -49,80 +61,141 @@ const Ulceras = () => {
               <img
                 src={woundsAssets.ulceras_imagen_4.content}
                 alt={woundsAssets.ulceras_imagen_4.alt}
-                style={{ width: "100%" }}
+                style={{
+                  width: "97%",
+                  marginLeft: "54.3px",
+                  marginTop: "1.2%",
+                  height: "774px",
+                }}
               />
             </StyledTerapeuticNumberSection1>
-            <StyledSubtitle style={{ marginBottom: "3%" }}>
+            {/* PRESION */}
+            <StyledSubtitle
+              style={{
+                marginBottom: "3%",
+                marginTop: "3%",
+                fontSize: "28.3px",
+                marginLeft: "5%",
+              }}
+            >
               {woundsAssets.ulceras_titulo_1.content}
             </StyledSubtitle>
-            <p style={{ fontSize: "20px" }}>
+            <p
+              style={{
+                fontSize: "20px",
+                lineHeight: "1",
+                padding: "0 58px",
+                marginTop: "-1%",
+              }}
+            >
               {woundsAssets.ulceras_text_1.content}
             </p>
             <Grid container className="justify-center">
               <img
                 src={woundsAssets.ulceras_imagen_6.content}
                 alt={woundsAssets.ulceras_imagen_6.alt}
-                style={{ width: "75%", marginTop: "3%" }}
+                style={{
+                  width: "67.3%",
+                  marginTop: "1.4%",
+                  marginLeft: "4.4%",
+                }}
               />
             </Grid>
-            <StyledSubtitle style={{ marginBottom: "3%" }}>
+            {/* CLASIFICACION */}
+            <StyledSubtitle
+              style={{
+                marginBottom: "3%",
+                marginTop: "5.7%",
+                fontSize: "28.4px",
+                marginLeft: "5.2%",
+              }}
+            >
               {woundsAssets.ulceras_titulo_3.content}
             </StyledSubtitle>
             <p
               dangerouslySetInnerHTML={{
                 __html: woundsAssets.ulceras_text_2.content,
               }}
-              style={{ fontSize: "20px" }}
+              style={{
+                fontSize: "20px",
+                lineHeight: "1",
+                padding: "0 57px",
+                marginTop: "-0.3%",
+              }}
             ></p>
             <Grid container className="justify-center">
               <img
                 src={woundsAssets.ulceras_imagen_7.content}
                 alt={woundsAssets.ulceras_imagen_7.alt}
-                style={{ width: "75%", marginTop: "3%" }}
+                style={{ width: "95%", marginTop: "3%", marginLeft: "3%" }}
               />
             </Grid>
-            <StyledSubtitle style={{ marginBottom: "3%" }}>
+            {/* CLASIFICACION */}
+
+            {/* prevenir lesiones */}
+            <StyledSubtitle
+              style={{
+                marginBottom: "3%",
+                marginTop: "2.8%",
+                fontSize: "28px",
+                marginLeft: "5.3%",
+              }}
+            >
               {woundsAssets.ulceras_titulo_4.content}
             </StyledSubtitle>
             <p
               dangerouslySetInnerHTML={{
                 __html: woundsAssets.ulceras_text_8.content,
               }}
-              style={{ fontSize: "20px" }}
+              style={{
+                fontSize: "20px",
+                lineHeight: "1",
+                padding: "0 58px",
+                marginTop: "-1%",
+              }}
             ></p>
             <Grid container className="justify-center">
               <img
                 src={woundsAssets.ulceras_imagen_9.content}
                 alt={woundsAssets.ulceras_imagen_9.alt}
-                style={{ width: "75%", marginTop: "3%" }}
+                style={{ width: "97%", marginTop: "1.9%", marginLeft: "3%" }}
               />
               <img
                 src={woundsAssets.ulceras_imagen_10.content}
                 alt={woundsAssets.ulceras_imagen_10.alt}
-                style={{ width: "75%", marginTop: "2%" }}
+                style={{ width: "97%", marginTop: "-0.1%", marginLeft: "3%" }}
               />
             </Grid>
           </Container>
-          {woundsAssets.ulceras_refrencias_1 !== undefined && (
+          {/* REFERENCIAS */}
+          {woundsAssets.references !== undefined && (
             <div style={{ background: "#014380" }} className="py-12">
               <Container maxWidth="lg">
                 <h1
                   className="pb-12 text-center text-white"
-                  style={{ fontSize: 30, fontWeight: "700" }}
+                  style={{
+                    fontSize: 30,
+                    fontWeight: "700",
+                    marginTop: "-3%",
+                    marginLeft: "5%",
+                  }}
                 >
                   Referencias
                 </h1>
-                <p
-                  dangerouslySetInnerHTML={{
-                    __html: woundsAssets.ulceras_refrencias_1.content,
-                  }}
-                  className="text-white"
-                ></p>
               </Container>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: woundsAssets.references.content,
+                }}
+                className="text-white text-xs"
+                style={{ padding: "0 160px", marginTop: "-1.2%" }}
+              ></p>
             </div>
           )}
+          {/* REFERENCIAS */}
         </React.Fragment>
       )}
+      {/* ULCERAS  */}
     </React.Fragment>
   );
 };
